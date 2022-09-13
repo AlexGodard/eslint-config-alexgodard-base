284d3a59476e45ccc6fd5cad58dc6a84df752564
Alexandre Godard <alexandre@godard.me>
2022-09-12 21:42:01 -0400

    Disable some rules

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ module.exports = {
     },
     "overrides": [
         {
-            "files": ["**/*.test.ts", "**/*.spec.ts", "**/*.e2e.ts", "**/test/**/*.ts"],
+            "files": ["**/*.test.ts", "**/*.spec.ts", "**/*.e2e.ts", "**/test/**/*.ts", "**/*.cy.*", '**/*.config.*'],
             "rules": {
                 "import/no-extraneous-dependencies": 0,
                 "no-unused-expressions": 0,
